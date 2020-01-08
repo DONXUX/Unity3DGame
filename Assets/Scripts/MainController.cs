c3f380619c94d895408493605f3fe61284b009be
donguk <ldu2175@gmail.com>
2020-01-08 11:24:57 +0900

    Add Chan
    Add Chan

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.Play("JUMP00", -1, 0);
+        }
+    }
+}
